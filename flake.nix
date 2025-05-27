e6cd5e6e5247eee09e15af3754479b81dfad52f6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-27 14:29:09 +0100

    chore: bump 0.7.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         
         zigchat = pkgs.stdenv.mkDerivation {
           pname = "zigchat";
-          version = "0.6.0";
+          version = "0.7.0";
           
           src = ./.;
           
